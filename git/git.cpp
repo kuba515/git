c37c7dd639351f8ef6c6bd4132c657caf111afd0
kuba515 <29944@s.pm.szczecin.pl>
2023-01-10 11:01:57 +0100

    bomba

--- git/git.cpp
+++ git/git.cpp
@@ -60,7 +60,27 @@ int** tab;
 		}
 	}
 	
+	void losuj_pozycje()
+	{
+		time_t t;
+		int poz_x, poz_y;
+		int ilosc = 10;
+		string plansza;
+		string ustaw_mine;
+		srand((unsigned)time(&t));
 
+		while (ilosc > 0)
+		{
+			poz_x = rand() % 10;
+			poz_y = rand() % 10;
+
+			if ( plansza[poz_x][poz_y].wartosc != 9)
+			{
+				 ustaw_mine(poz_x, poz_y);
+				ilosc--;
+			}
+		}
+	}
 
 
 
