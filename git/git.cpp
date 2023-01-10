188b07fb5d9116fe23b671cb1b06dcc3396f8766
kuba515 <29944@s.pm.szczecin.pl>
2023-01-10 10:51:00 +0100

    menu

--- git/git.cpp
+++ git/git.cpp
@@ -1,6 +1,6 @@
 ﻿// git.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
-
+#include <locale.h>
 #include <iostream>
 #include <conio.h>
 using namespace std;
@@ -67,22 +67,38 @@ int** tab;
 
  
 
-int main()
-{
-	wypiszdanelt();
-	wypiszdanest();
-	wypiszdanett();
+	int main()
+	{
+		setlocale(LC_CTYPE, "Polish");
+
+		
 
 
-}
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+
+		int x = 0;
+		while (x > 3 || x < 1)
+		{
+			cout << "*******************MENU SAPER********************" << endl;
+			cout << "1.Łatwa plansza" << endl;
+			cout << "2.Średnia plansza" << endl;
+			cout << "3.Trudna plansza" << endl;
+			cout << "Wybieram : ";
+			cin >> x;
+			system("cls");
+		}
+
+		switch (x)
+		{
+		case 1:
+			wypiszdanelt();
+			break;
+		case 2:
+			wypiszdanest();
+			break;
+		case 3:
+			wypiszdanett();
+			break;
+		}
+	}
