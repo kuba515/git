9440b1815b14e8cce84a12867369f585e8f02e83
Jakub Kuczyński <kuba.kuczx@wp.pl>
2023-02-07 00:02:29 +0100

    miny

--- git/git.cpp
+++ git/git.cpp
@@ -68,4 +68,177 @@ void makeFlag(int* x, int* y)
 	else {
 		flag[fx][fb] = true;
 	}
+}
+void printBoard(char myBoard[][MAXSIDE])
+{
+	int i, j;
+
+	cout << (" ");
+
+	for (i = 0; i < SIDE; i++)
+		cout << "\t" << i;
+
+	cout << ("\n");
+
+	for (i = 0; i < SIDE; i++)
+	{
+		cout << i << "\t";
+
+		for (j = 0; j < SIDE; j++)
+			if (flag[i][j] == true) {
+				cout << "X\t";
+			}
+			else {
+				cout << myBoard[i][j] << "\t";
+			}
+
+		cout << ("\n");
+	}
+}
+
+
+int countAdjacentMines(int row, int col, int mines[][2],
+	char realBoard[][MAXSIDE])
+{
+
+	int i;
+	int count = 0;
+
+
+
+	if (isValid(row - 1, col) == true)
+	{
+		if (isMine(row - 1, col, realBoard) == true)
+			count++;
+	}
+
+	if (isValid(row + 1, col) == true)
+	{
+		if (isMine(row + 1, col, realBoard) == true)
+			count++;
+	}
+
+	if (isValid(row, col + 1) == true)
+	{
+		if (isMine(row, col + 1, realBoard) == true)
+			count++;
+	}
+
+	if (isValid(row, col - 1) == true)
+	{
+		if (isMine(row, col - 1, realBoard) == true)
+			count++;
+	}
+
+	if (isValid(row - 1, col + 1) == true)
+	{
+		if (isMine(row - 1, col + 1, realBoard) == true)
+			count++;
+	}
+
+	if (isValid(row - 1, col - 1) == true)
+	{
+		if (isMine(row - 1, col - 1, realBoard) == true)
+			count++;
+	}
+
+	if (isValid(row + 1, col + 1) == true)
+	{
+		if (isMine(row + 1, col + 1, realBoard) == true)
+			count++;
+	}
+
+	if (isValid(row + 1, col - 1) == true)
+	{
+		if (isMine(row + 1, col - 1, realBoard) == true)
+			count++;
+	}
+
+	return (count);
+}
+
+
+bool playMinesweeperUtil(char myBoard[][MAXSIDE], char realBoard[][MAXSIDE],
+	int mines[][2], int row, int col, int* movesLeft)
+{
+
+	if (myBoard[row][col] != '-')
+		return (false);
+
+	int i, j;
+
+
+	if (realBoard[row][col] == '*')
+	{
+		myBoard[row][col] = '*';
+
+		for (i = 0; i < MINES; i++)
+			myBoard[mines[i][0]][mines[i][1]] = '*';
+
+		printBoard(myBoard);
+		cout << ("\nYou lost!\n");
+		return (true);
+	}
+
+	else
+	{
+		int count = countAdjacentMines(row, col, mines, realBoard);
+		(*movesLeft)--;
+
+		myBoard[row][col] = count + '0';
+
+		if (!count)
+		{
+			if (isValid(row - 1, col) == true)
+			{
+				if (isMine(row - 1, col, realBoard) == false)
+					playMinesweeperUtil(myBoard, realBoard, mines, row - 1, col, movesLeft);
+			}
+
+			if (isValid(row + 1, col) == true)
+			{
+				if (isMine(row + 1, col, realBoard) == false)
+					playMinesweeperUtil(myBoard, realBoard, mines, row + 1, col, movesLeft);
+			}
+
+			if (isValid(row, col + 1) == true)
+			{
+				if (isMine(row, col + 1, realBoard) == false)
+					playMinesweeperUtil(myBoard, realBoard, mines, row, col + 1, movesLeft);
+			}
+
+			if (isValid(row, col - 1) == true)
+			{
+				if (isMine(row, col - 1, realBoard) == false)
+					playMinesweeperUtil(myBoard, realBoard, mines, row, col - 1, movesLeft);
+			}
+
+			if (isValid(row - 1, col + 1) == true)
+			{
+				if (isMine(row - 1, col + 1, realBoard) == false)
+					playMinesweeperUtil(myBoard, realBoard, mines, row - 1, col + 1, movesLeft);
+			}
+
+			if (isValid(row - 1, col - 1) == true)
+			{
+				if (isMine(row - 1, col - 1, realBoard) == false)
+					playMinesweeperUtil(myBoard, realBoard, mines, row - 1, col - 1, movesLeft);
+			}
+
+			if (isValid(row + 1, col + 1) == true)
+			{
+				if (isMine(row + 1, col + 1, realBoard) == false)
+					playMinesweeperUtil(myBoard, realBoard, mines, row + 1, col + 1, movesLeft);
+			}
+
+			if (isValid(row + 1, col - 1) == true)
+			{
+				if (isMine(row + 1, col - 1, realBoard) == false)
+					playMinesweeperUtil(myBoard, realBoard, mines, row + 1, col - 1, movesLeft);
+			}
+		}
+
+		return (false);
+	}
+}
 
