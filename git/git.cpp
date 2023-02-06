6450d157727aec894426620e4953bbca5ca79b2b
Jakub Kuczyński <kuba.kuczx@wp.pl>
2023-02-06 22:45:04 +0100

    xd

--- git/git.cpp
+++ git/git.cpp
@@ -1,124 +1,55 @@
 ﻿// git.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
-#include <locale.h>
+
 #include <iostream>
-#include <conio.h>
+
+using namespace std;
+
+#define BEGINNER 0
+#define INTERMEDIATE 1
+#define ADVANCED 2
+#define MAXSIDE 25
+#define MAXMINES 99
+#define MOVESIZE 526 // (25 * 25 - 99)
+
+int SIDE;
+int MINES;
+bool flag[24][24];
+
 using namespace std;
 
 
-// bomba
-int** tab;
-	int tabl[8][8];
-	int tabs[16][16];
-	int tabt[32][16];
-	//ml-latwa tablica,ms-srednia tablica,mt-truDNA TABLICA
-	void wypiszdanelt() {
-		for (int i = 0; i < 8; i++)
-		{
-			for (int j = 0; j < 8; j++) {
-				tabl[i][j] = i * j;
-			}
-		}
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				cout << tabl[i][j] << "\t";
-			}
-			cout << endl;
-
-		}
-		
-
-	}
-	void wypiszdanest() {
-		for (int h = 8; h < 16; h++) {
-
-			for (int g = 0; g < 16; g++) {
-				tabs[h][g] = h * g;
-
-			}
-		}
-		for (int h = 0; h < 16;h++) {
-			for (int g = 0; g < 16; g++) {
-				cout << tabs[h][g] << "\t";
-			}
-			cout << endl;
-
-		}
-
-	}
-	void wypiszdanett() {
-		for (int a = 0; a < 32; a++) {
-			for (int b = 0; b < 16; b++) {
-				tabt[a][b] = a * b;
-			}
-		}
-		for (int a = 0; a < 32; a++) {
-			for (int b = 0; b < 16; b++) {
-				cout << tabt[a][b] << "\t";
-			}
-			cout << endl;
+void clearFLags() {
+	for (int i = 0; i < 24; i++) {
+		for (int j = 0; j < 24; j++) {
+			flag[i][j] = false;
 		}
 	}
-	
-	void losuj_pozycje()
-	{
-		time_t t;
-		int poz_x, poz_y;
-		int ilosc = 10;
-		string plansza;
-		string ustaw_mine;
-		srand((unsigned)time(&t));
+}
 
-		while (ilosc > 0)
-		{
-			poz_x = rand() % 10;
-			poz_y = rand() % 10;
+bool isValid(int row, int col)
+{
 
-			if ( plansza[poz_x][poz_y].wartosc != 9)
-			{
-				 ustaw_mine(poz_x, poz_y);
-				ilosc--;
-			}
-		}
-	}
+	return (row >= 0) && (row < SIDE) &&
+		(col >= 0) && (col < SIDE);
+}
 
 
+bool isMine(int row, int col, char board[][MAXSIDE])
+{
+	if (board[row][col] == '*')
+		return (true);
+	else
+		return (false);
+}
 
 
- 
-
-	int main()
-	{
-		setlocale(LC_CTYPE, "Polish");
-
-		
-
-
-
-
-
-		int x = 0;
-		while (x > 3 || x < 1)
-		{
-			cout << "*******************MENU SAPER********************" << endl;
-			cout << "1.Łatwa plansza" << endl;
-			cout << "2.Średnia plansza" << endl;
-			cout << "3.Trudna plansza" << endl;
-			cout << "Wybieram : ";
-			cin >> x;
-			system("cls");
-		}
-
-		switch (x)
-		{
-		case 1:
-			wypiszdanelt();
-			break;
-		case 2:
-			wypiszdanest();
-			break;
-		case 3:
-			wypiszdanett();
-			break;
-		}
-	}
+void makeMove(int* x, int* y)
+{
+	int a, b;
+	cout << "Wpisz x i y nastepnego ruchu -> " << endl;
+	cin >> a;
+	cin >> b;
+	*x = a;
+	*y = b;
+}
